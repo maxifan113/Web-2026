f3646c6e52fc86f286af66a1e31af050b44d47fd
maxifan113 <maxifan113@gmail.com>
2026-04-07 17:39:40 +0300

    Lab4_1-2

---
+++ Lab4/DigitToWord/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>DigitToWord</title>
+        <meta charset="UTF-8">
+        <link href="./style.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Перевод цифры в слово</h1>
+    <form method="POST" >
+      <label for="digit">Введите цифру 0т 0 до 9</label>
+      <input type="number" name="digit" min="0" max="9" id="digit" required>
+      <button type="submit">Перевести</button>
+    </form>
+
+    <?php
+    function digitToWord($digit): string
+    {
+      switch ($digit) 
+      {
+        case 0:
+            return 'Ноль';
+        case 1:
+            return 'Один';
+        case 2:
+            return 'Два';
+        case 3:
+            return 'Три';
+        case 4:
+            return 'Четыре';
+        case 5:
+            return 'Пять';
+        case 6:
+            return 'Шесть';
+        case 7:
+            return 'Семь';
+        case 8:
+            return 'Восемь';
+        case 9:
+            return 'Девять';
+        default:
+            return 'Неверная цифра';
+      }
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+      $digit = $_POST['digit'];
+      $result = 'Вы ввели цифру: ' . digitToWord($digit);
+      echo $result;
+    }
+    ?>
+  </body>
+</html>
---
+++ Lab4/VsikosnyGod/cheak.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>Результат</title>
+        <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1>Результат</h1>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+      $year = (int)$_POST['year'];
+      if (is_numeric($year) && $year > 0)
+      {
+        if ($year % 4 == 0)
+        {
+          echo 'Год високосный';
+        }
+        else
+        {
+          echo 'Год невисокосный';
+        }
+      }
+      elseif ($year == 0)
+      {
+        echo 'Вы не ввели год';
+      }
+
+    }
+    ?>
+    <br>
+    <a href="index.html">Проверить другой год</a>
+  </body>
+</html>
